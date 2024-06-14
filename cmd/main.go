eeef423523ce7e74c300999d60309867f8531e31
AlecSmith96 <alec.smith@fourthfloorcreative.co>
2024-06-14 19:38:56 +0100

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/AlecSmith96/adapters"
+)
+
+func main() {
+	router := adapters.NewRouter()
+}
---
+++ internal/drivers/router.go
@@ -0,0 +1,9 @@
+package drivers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine {
+
+}
